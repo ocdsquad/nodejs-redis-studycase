48d17153b1689f8e6c772c47e9e8631b99fca300
ahmad alfarel <assaniry202@gmail.com>
2025-07-28 08:56:57 +0700

    feat: add redis for better performance

--- src/service/category-service.js
+++ src/service/category-service.js
@@ -1,8 +1,25 @@
+import Redis from "ioredis";
 import { prismaClient } from "../application/database.js";
+
+const redis = new Redis({
+    host: "localhost",
+    port: 6379,
+    db: 0
+});
 
 const findAll = async (req, res, next) => {
     // query all categories parents
     try {
+        /*
+        check apakah data ada di redis?
+        jika ada, kembalikan langsung data ke user
+        jika tidak, query ke database lalu simpan ke redis 
+        */
+
+        const json = await redis.get("categories");
+
+        if(json) return JSON.parse(json);
+
         const parents = await prismaClient.category.findMany({
             where: { parent_id: null },
             select: {
@@ -13,6 +30,9 @@ const findAll = async (req, res, next) => {
         });
 
 
+        //simpan ke redis
+        await redis.setex("categories", 60*60, JSON.stringify(parents));
+        
         // for (let parent of parents) {
         //     // query all categories children
         //     parent.children = await prismaClient.category.findMany({
